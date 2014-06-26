70ede28f4e505bb4f4f29d486834163aaa1f6456
Greg Cosgrove <gregcosgrove@gmail.com>
2014-06-26 14:26:30 -0500

    Resync

--- spec/views/challenges/index.html.erb_spec.rb
+++ spec/views/challenges/index.html.erb_spec.rb
@@ -16,7 +16,7 @@ describe "challenges/index" do
 
   it "renders a list of challenges" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Title".to_s, :count => 2
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
   end
