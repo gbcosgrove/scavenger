a3a1cd72c84fe7207f004526c312a21e465261af
Greg Cosgrove <gregcosgrove@gmail.com>
2014-06-26 01:08:19 -0500

    Add validations and tests for creating challenge items and style flash messages

--- app/models/challenge_item.rb
+++ app/models/challenge_item.rb
@@ -1,3 +1,6 @@
 class ChallengeItem < ActiveRecord::Base
   belongs_to :challenge
+
+  validates :content, presence: true,
+                      length: { minimum: 3 }
 end
--- spec/features/challenge_items/create_spec.rb
+++ spec/features/challenge_items/create_spec.rb
@@ -21,5 +21,26 @@ describe "Adding challenge items" do
     end
   end
 
+  it "displays an error with no content" do
+    visit_challenge(challenge)
+    click_link "New Challenge Item"
+    fill_in "Content", with: ""
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was an error adding that challenge item")
+    end
+    expect(page).to have_content("Content can't be blank")
+  end
+
+  it "displays an error with content less than 3 characters long" do
+    visit_challenge(challenge)
+    click_link "New Challenge Item"
+    fill_in "Content", with: "Hi"
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was an error adding that challenge item")
+    end
+    expect(page).to have_content("Content is too short")
+  end
 
 end
