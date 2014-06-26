d7bc889e0b9b6e9e6a9633797e1236ecf75fa8c8
Greg Cosgrove <gregcosgrove@gmail.com>
2014-06-26 16:27:19 -0500

    Add user validations for emails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) {
+    {
+      first_name: "Greg",
+      last_name: "Cosgrove",
+      email: "gregcosgrove@gmail.com"
+    }
+  }
+  context "validations" do
+    let(:user) { User.new(valid_attributes) }
+
+    before do
+      User.create(valid_attributes)
+    end
+
+    it "requires an email" do
+      expect(user).to validate_presence_of(:email)
+    end
+
+    it "requires a unique email" do
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
+    it "requires a unique email (case insensitive)" do
+      user.email = "JOEBLOGGS@GMAIL.COM"
+      expect(user).to validate_uniqueness_of(:email)
+    end
+  end
+
+  context "#downcase_email" do
+    it "makes the email attribute lower case" do
+      user = User.new(valid_attributes.merge(email: "JOEBLOGGS@GMAIL.COM"))
+      expect{ user.downcase_email }.to change{ user.email }.
+        from("JOEBLOGGS@GMAIL.COM").
+        to("joebloggs@gmail.com")
+    end
+
+    it "downcases an email before saving" do
+      user = User.new(valid_attributes)
+        user.email = "GREGCOSGROVE@GMAIL.COM"
+        expect(user.save).to be_true
+        expect(user.email).to eq("gregcosgrove@gmail.com")
+      end
+  end
+
 end
